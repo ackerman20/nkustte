96eab32061fc87f86e974b6448c5a46c9e97a8ea
ackerman20 <73731858+ackerman20@users.noreply.github.com>
2022-12-15 00:35:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="zh-tw">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <title>電訊工程系分享網站</title>
+</head>
+<body>
+  <div class="header">
+    <h1>電訊工程系分享網站</h1>
+  </div>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/nkustte/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="RF_Circuit_Design.php">射頻電路設計</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+  <div class="row">
+    <div class="col-3 col-s-3 menu">
+      <ul>
+        <li>電訊工程系</li>
+        <li>英文縮寫 : TE</li>
+        <li>跨年倒數 : 16天</li>
+      </ul>
+    </div>
+
+    <div class="col-6 col-s-9">
+      <h1>歡迎來到這個網站</h1>
+      <p>這裡為非系上共享專區，選擇想看的科目內容，如果找不到歡迎提供。</p>
+    </div>
+
+    <div class="col-3 col-s-12">
+      <div class="aside">
+        <h2>廣告區</h2>
+        <p>這裡可能會放置廣告</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer">
+    <p>創建於2022年12月</p>
+    <p>聯繫信箱 : C108185129@nkust.edu.tw 最後更新 : 2022/12/15</p>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
